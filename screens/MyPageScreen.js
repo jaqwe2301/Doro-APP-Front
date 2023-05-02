08e3edeb3e363d0cc7d92ef1ecfd217b8c8f24e5
Noh Sein <si2238@naver.com>
2023-05-02 19:58:30 +0900

    Feat: Add MyPage Style

--- screens/MyPageScreen.js
+++ screens/MyPageScreen.js
@@ -1,7 +1,194 @@
-import { Text } from "react-native";
+import {
+  ScrollView,
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  Pressable,
+} from "react-native";
+import { GlobalStyles } from "../constants/styles";
 
 function MyPageScreen() {
-  return <Text>MyPage</Text>;
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image
+            style={styles.image}
+            source={require("../assets/profile.png")}
+          />
+          <View style={styles.statusContainer}>
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>강의 신청</Text>
+              <Text style={styles.textNum}>02</Text>
+            </View>
+            <View style={[styles.textContainer, { marginHorizontal: 26 }]}>
+              <Text style={styles.text}>배정 완료</Text>
+              <Text style={styles.textNum}>02</Text>
+            </View>
+            <View style={styles.textContainer}>
+              <Text style={styles.text}>강의 완료</Text>
+              <Text style={styles.textNum}>02</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ marginHorizontal: 20, marginTop: 6 }}>
+          <Text style={styles.name}>김혜원</Text>
+          <Text style={styles.title}>DORO 3기</Text>
+        </View>
+        <View
+          style={{
+            marginHorizontal: 20,
+            marginTop: 16,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View style={[styles.btnContainer, { marginRight: 10 }]}>
+            <Pressable>
+              <Text style={styles.btn}>프로필 편집</Text>
+            </Pressable>
+          </View>
+          <View style={styles.btnContainer}>
+            <Pressable>
+              <Text style={styles.btn}>강의신청내역</Text>
+            </Pressable>
+          </View>
+        </View>
+        <View>
+          <Text style={[styles.contentTitle, { marginTop: 45 }]}>기본정보</Text>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>생년월일</Text>
+            <Text style={styles.contentText}>2001.09.27</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>휴대전화번호</Text>
+            <Text style={styles.contentText}>010-4478-1672</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>학교</Text>
+            <Text style={styles.contentText}>한양대학교</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>전공</Text>
+            <Text style={styles.contentText}>ICT융합학부</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>학번</Text>
+            <Text style={styles.contentText}>202004257</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>재학 유무</Text>
+            <Text style={styles.contentText}>재학중</Text>
+          </View>
+          <View style={styles.border}></View>
+        </View>
+        <View>
+          <Text style={styles.contentTitle}>로그인 정보</Text>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>아이디</Text>
+            <Text style={styles.contentText}>hynnk0</Text>
+          </View>
+          <View style={styles.contentContainer}>
+            <Text style={styles.title}>비밀번호</Text>
+            <Pressable>
+              <Text style={[styles.contentText, { borderBottomWidth: 1 }]}>
+                비밀번호 수정
+              </Text>
+            </Pressable>
+          </View>
+          <View style={styles.border}></View>
+        </View>
+        <View style={{ marginBottom: 33 }}>
+          <Text
+            style={[styles.contentTitle, { borderBottomWidth: 1, width: 57 }]}
+          >
+            로그아웃
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 export default MyPageScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  image: {
+    width: 99,
+    marginLeft: 20,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: 400,
+    lineHeight: 17,
+  },
+  textNum: {
+    fontSize: 15,
+    fontWeight: 400,
+    lineHeight: 20,
+    marginTop: 17,
+  },
+  textContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  statusContainer: {
+    flexDirection: "row",
+    marginLeft: 30,
+  },
+  name: {
+    fontWeight: 600,
+    fontSize: 17,
+    lineHeight: 22,
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 400,
+    lineHeight: 17,
+    width: 70,
+    color: GlobalStyles.colors.gray03,
+  },
+  btnContainer: {
+    height: 30,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: GlobalStyles.colors.gray05,
+    borderWidth: 0.5,
+    borderRadius: 5.41,
+    backgroundColor: "white",
+    elevation: 3,
+  },
+  btn: {
+    fontSize: 10,
+    fontWeight: 400,
+    lineHeight: 20,
+  },
+  contentContainer: {
+    flexDirection: "row",
+    marginTop: 33,
+    marginHorizontal: 20,
+  },
+  contentText: {
+    marginLeft: 45,
+    fontSize: 12,
+    fontWeight: 400,
+    lineHeight: 17,
+  },
+  contentTitle: {
+    marginHorizontal: 20,
+    fontSize: 15,
+    fontWeight: 600,
+    lineHeight: 20,
+  },
+  border: {
+    borderBottomColor: GlobalStyles.colors.gray05,
+    borderBottomWidth: 1,
+    marginHorizontal: 20,
+    marginVertical: 33,
+  },
+});
