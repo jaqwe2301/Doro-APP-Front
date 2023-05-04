1eecff195ad20ae76d8e32c0a403e4d2937ab6d5
jaqwe2301 <jaqwe2301@naver.com>
2023-05-05 02:23:27 +0900

    Fix: [홈] - [모집중] Api에 맞게 status라는 변수, 값 수정/ mypage 병합

---
+++ components/ui/ButtonOneThird.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+
+function ButtonOneThird({ text, style, onPress }) {
+  return (
+    <Pressable onPress={onPress}>
+      <View style={[styles.container, { backgroundColor: style }]}>
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default ButtonOneThird;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 49,
+    elevation: 3,
+    borderRadius: 5.41,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: GlobalStyles.colors.gray07,
+    fontSize: 17,
+    fontWeight: 600,
+    lineHeight: 22,
+  },
+});
