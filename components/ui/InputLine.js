836b67d1c8cee2d58abe366b125a88fb76c2edc6
Noh Sein <si2238@naver.com>
2023-05-09 17:29:17 +0900

    Fix: Add Profile api

--- components/ui/InputLine.js
+++ components/ui/InputLine.js
@@ -23,15 +23,14 @@ export default InputLine;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginLeft: 45,
     borderBottomColor: GlobalStyles.colors.gray06,
     borderBottomWidth: 0.5,
-    marginLeft: 45,
     height: 17,
   },
   text: {
     fontSize: 12,
     fontWeight: "400",
-    lineHeight: 1,
     color: GlobalStyles.colors.gray01,
   },
 });
