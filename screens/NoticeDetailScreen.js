acaa3d9e1e3b4e99e5ce4c499662cd1357e61e2e
Noh Sein <si2238@naver.com>
2023-05-11 01:03:11 +0900

    Feat; Add NoticeDetailScreen

--- screens/NoticeDetailScreen.js
+++ screens/NoticeDetailScreen.js
@@ -8,7 +8,15 @@ function NoticeDetailScreen({ navigation, route }) {
     <View style={styles.container}>
       <View style={styles.headerBar} />
       <ScrollView>
-        <Text>{targetData.title}</Text>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>{targetData.title}</Text>
+          <View style={styles.nameContainer}>
+            <Text style={styles.name}>{targetData.name} 매니저</Text>
+            <Text style={styles.name}>{targetData.date}</Text>
+          </View>
+
+          <Text style={styles.subcontentContainer}>{targetData.content}</Text>
+        </View>
       </ScrollView>
     </View>
   );
@@ -24,5 +32,32 @@ const styles = StyleSheet.create({
   headerBar: {
     borderBottomColor: GlobalStyles.colors.gray04,
     borderBottomWidth: 0.8,
+  },
+  contentContainer: {
+    margin: 20,
+  },
+  title: {
+    fontWeight: 600,
+    fontSize: 22,
+    lineHeight: 28,
+    color: GlobalStyles.colors.gray01,
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+  },
+  name: {
+    fontSize: 12,
+    fontWeight: 400,
+    lineHeight: 17,
+    color: GlobalStyles.colors.gray03,
+  },
+  subcontentContainer: {
+    marginTop: 20,
+    fontSize: 17,
+    fontWeight: 400,
+    lineHeight: 22,
+    color: GlobalStyles.colors.gray03,
   },
 });
