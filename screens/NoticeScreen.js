667e9e44deac93c7b4140abb9276094d9b00d4db
Noh Sein <si2238@naver.com>
2023-05-12 17:12:53 +0900

    Feat: Add Notification api

--- screens/NoticeScreen.js
+++ screens/NoticeScreen.js
@@ -9,102 +9,45 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GlobalStyles } from "../constants/styles";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { HeaderContext } from "../store/header-context";
+import { getNotification } from "../utill/http";
+import moment from "moment";
 
 function NoticeScreen({ navigation }) {
   const { headerRole, setHeaderRole } = useContext(HeaderContext);
-  const DATA = [
-    {
-      id: "1",
-      title: "용인쌤과 함께하는 폰카메라 보정 마스터 워크숍 안내",
-      name: "김동규",
-      date: "2023-04-02",
-    },
-    {
-      id: "2",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "3",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      content:
-        "목적이란 어떤 것을 하는 목표를 의미한다. 어떤 국어사전에서는 목적에 대하여 다음과 같이 정의하고 있다. 일을 이루려고 하는 목표나 나아가는 방향 목적은 철학적으로도 '살아가는 목적' 등 따위로 넓게 연구되고 있다.\n이 정의들만 봐도 감이 오시죠? 목적은 실현하려는 일 또는 상태, 목표는 어떤 목적을 실현하기 위한 정량적 지표를 뜻합니다.",
-      date: "2023-04-05",
-    },
-    {
-      id: "4",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "5",
-      title: "용인쌤과 함께하는 폰카메라 보정 마스터 워크숍 안내",
-      name: "김동규",
-      date: "2023-04-02",
-    },
-    {
-      id: "6",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "7",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "8",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "9",
-      title: "용인쌤과 함께하는 폰카메라 보정 마스터 워크숍 안내",
-      name: "김동규",
-      date: "2023-04-02",
-    },
-    {
-      id: "10",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "11",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-    {
-      id: "12",
-      title: "강사 배정에 관한 규정 변경에 대한 안내",
-      name: "김동규",
-      date: "2023-04-05",
-    },
-  ];
+  const [data, setData] = useState([]);
 
-  function navigHandler() {
-    navigation.navigate("noticeDetail", { data: DATA });
+  async function notiHandler() {
+    try {
+      const response = await getNotification();
+      setData(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  useEffect(() => {
+    notiHandler();
+  }, []);
+
+  const navigHandler = (item) => {
+    navigation.navigate("noticeDetail", { data: item });
+  };
   function naviAddHandler() {
     navigation.navigate("noticeAdd");
   }
 
-  const Item = ({ title, name, date }) => (
+  const Item = ({ item }) => (
     <View style={styles.content}>
-      <Pressable onPress={navigHandler}>
-        <Text style={styles.title}>{title}</Text>
+      <Pressable onPress={() => navigHandler(item)}>
+        <Text style={styles.title}>{item.title}</Text>
         <View style={styles.subContent}>
-          <Text style={styles.name}>{name} 매니저</Text>
-          <Text style={styles.name}>{date}</Text>
+          <Text style={styles.name}>김동규 매니저</Text>
+          <Text style={styles.name}>
+            {moment(item.date).format("YYYY-MM-DD")}
+          </Text>
         </View>
       </Pressable>
     </View>
@@ -114,10 +57,8 @@ function NoticeScreen({ navigation }) {
     <View style={styles.container}>
       <View style={styles.headerBar} />
       <FlatList
-        data={DATA}
-        renderItem={({ item }) => (
-          <Item title={item.title} name={item.name} date={item.date} />
-        )}
+        data={data.reverse()}
+        renderItem={Item}
         keyExtractor={(item) => item.id}
       />
       {headerRole === "ROLE_ADMIN" ? (
