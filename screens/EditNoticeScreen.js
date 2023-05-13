03c41b26d2bc74011ca7530c1743f427eefc2015
Noh Sein <si2238@naver.com>
2023-05-14 00:24:59 +0900

    Feat: Add EditAnnouncement Api

--- screens/EditNoticeScreen.js
+++ screens/EditNoticeScreen.js
@@ -1,23 +1,74 @@
-import { View, Text, StyleSheet, ScrollView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Pressable,
+} from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import moment from "moment";
+import { useEffect, useState } from "react";
+import { editAnnouncement } from "../utill/http";
 
 function EditNoticeScreen({ navigation, route }) {
   const data = route.params.data;
+  const [body, setBody] = useState(data.body);
+  const [title, setTitle] = useState(data.title);
+
+  async function completeHandler() {
+    try {
+      const response = await editAnnouncement({
+        title: title,
+        body: body,
+        picture: "",
+        id: data.id,
+      });
+
+      console.log(response);
+      if (response.success) {
+        navigation.replace("noticeScreen");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <Pressable onPress={completeHandler}>
+            <Text style={styles.completeText}>완료</Text>
+          </Pressable>
+        );
+      },
+    });
+  }, [completeHandler]);
+
   return (
     <View style={styles.container}>
       <View style={styles.headerBar} />
       <ScrollView>
         <View style={styles.contentContainer}>
-          <TextInput style={styles.title}>{data.title}</TextInput>
+          <TextInput
+            value={title}
+            style={styles.title}
+            multiline
+            onChangeText={(text) => setTitle(text)}
+          />
           <View style={styles.nameContainer}>
             <Text style={styles.name}>김동규 매니저</Text>
             <Text style={styles.name}>
               {moment(data.createdAt).format("YYYY-MM-DD")}
             </Text>
           </View>
-
-          <TextInput style={styles.subcontentContainer}>{data.body}</TextInput>
+          <TextInput
+            value={body}
+            onChangeText={(text) => setBody(text)}
+            style={styles.subcontentContainer}
+            multiline
+          />
         </View>
       </ScrollView>
     </View>
@@ -39,7 +90,7 @@ const styles = StyleSheet.create({
     margin: 20,
   },
   title: {
-    fontWeight: 600,
+    fontWeight: "600",
     fontSize: 22,
     lineHeight: 28,
     color: GlobalStyles.colors.gray01,
@@ -58,7 +109,7 @@ const styles = StyleSheet.create({
   subcontentContainer: {
     marginTop: 20,
     fontSize: 17,
-    fontWeight: 400,
+    fontWeight: "400",
     lineHeight: 22,
     color: GlobalStyles.colors.gray03,
   },
@@ -67,5 +118,20 @@ const styles = StyleSheet.create({
     position: "absolute",
     bottom: 16,
     right: 16,
+  },
+  completeText: {
+    fontWeight: "400",
+    fontSize: 15,
+    // lineHeight: 20,
+    width: 50,
+    height: 30,
+    borderRadius: 5.41,
+    color: "white",
+    textAlign: "center",
+    textAlignVertical: "center",
+    // marginLeft: -4,
+    backgroundColor: GlobalStyles.colors.primaryDefault,
+
+    lineHeight: 20,
   },
 });
