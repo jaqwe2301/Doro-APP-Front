ac5a66da94e030f8d623964d13b04dd0d00ad457
Noh Sein <si2238@naver.com>
2023-05-24 23:59:12 +0900

    Fix: refresh token

--- utill/Interceptor.js
+++ utill/Interceptor.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import { reToken } from "./auth";
+// import { reToken } from "./auth";
 
 function Interceptor() {
   const instance = axios.create({
@@ -10,13 +10,10 @@ function Interceptor() {
 
   instance.interceptors.request.use(
     async function (config) {
-      // const authCtx = useContext(AuthContext);
-
       const token = await AsyncStorage.getItem("token");
       console.log(token);
 
       if (token) {
-        //   config.headers["Content-Type"] = "application/json; charset=utf-8";
         config.headers["Authorization"] = `Bearer ${token}`;
       }
       return config;
@@ -29,21 +26,6 @@ function Interceptor() {
 
   instance.interceptors.response.use(
     async function (response) {
-      // console.log(response + "hihi");
-      // if (response.status === 401) {
-      //   const token = await AsyncStorage.getItem("token");
-      //   const refreshToken = await AsyncStorage.getItem("refreshToken");
-      //   console.log("re화이팅");
-
-      //   const reToken = await axios.post("http://10.0.2.2:8080/reissue", {
-      //     accessToken: token,
-      //     refreshToken: refreshToken,
-      //   });
-
-      //   // AsyncStorage.setItem("token",reToken)
-      //   console.log("hihi" + reToken);
-      // }
-
       return response;
     },
     async function (error) {
@@ -55,19 +37,16 @@ function Interceptor() {
         console.log(refreshToken);
 
         try {
-          const response = await reToken({
+          const response = await axios.post("http://10.0.2.2:8080/reissue", {
             accessToken: `Bearer ${token}`,
             refreshToken: refreshToken,
           });
-          console.log(response);
-
-          authCtx.authenticate(response, refreshToken);
+          const newToken = response.headers.authorization;
+          console.log("새로운 토큰이얌" + newToken);
+          AsyncStorage.setItem("token", newToken);
         } catch (error) {
           console.log("error발생" + error);
-          // console.log(error)
         }
-
-        // console.log(reToken);
       }
       return Promise.reject(error);
     }
