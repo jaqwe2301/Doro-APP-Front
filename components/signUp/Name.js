086125f633d427685fdbec67ddf26cec58238072
Nohsein <si2238@naver.com>
2023-06-27 12:24:15 +0900

    Feat: add ios signUp

--- components/signUp/Name.js
+++ components/signUp/Name.js
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { useState, useContext } from "react";
 
 import InputText from "../../components/ui/InputText";
@@ -9,11 +15,11 @@ import { useNavigation } from "@react-navigation/native";
 import Bar from "../ui/Bar";
 import { SignContext } from "../../store/sign-context";
 import InputData from "../ui/InputData";
-function Name() {
+function Name({ navigation, route }) {
   const [inputName, setInputName] = useState("");
   const [inputBirth, setInputBirth] = useState("");
   const [lbtnColor, setlbtnColor] = useState(GlobalStyles.colors.gray05);
-  const navigation = useNavigation();
+  const statusBarHeight = route.params.h;
   const { signData, setSignData } = useContext(SignContext);
 
   const [date, setDate] = useState(new Date(1598051730000));
@@ -66,7 +72,7 @@ function Name() {
     if (inputName !== "" && inputBirth !== "") {
       setSignData({ ...signData, name: inputName, birth: inputBirth });
 
-      navigation.navigate("school");
+      navigation.navigate("school", { h: statusBarHeight });
     } else {
     }
   }
@@ -74,34 +80,42 @@ function Name() {
   return (
     <View style={{ flex: 1, backgroundColor: "white" }}>
       <Bar num={2} />
-      <View style={{ flex: 1, justifyContent: "space-between" }}>
-        <View>
-          <View style={styles.textContainer}>
-            <InputText text="이름을 입력해 주세요." />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={
+          Platform.OS === "ios" ? 44 + statusBarHeight : 0
+        }
+      >
+        <View style={{ flex: 1, justifyContent: "space-between" }}>
+          <View>
+            <View style={styles.textContainer}>
+              <InputText text="이름을 입력해 주세요." />
+            </View>
+            <View style={styles.inputContainer}>
+              <InputData
+                hint="이름"
+                onChangeText={handleNameChange}
+                value={inputName}
+              />
+            </View>
+            <View style={[styles.textContainer, { marginTop: 50 }]}>
+              <InputText text="생년월일을 입력해 주세요." />
+            </View>
+            <View style={styles.inputContainer}>
+              <InputData
+                hint="생년월일"
+                onChangeText={handleBirthChange}
+                value={inputBirth}
+              />
+            </View>
           </View>
-          <View style={styles.inputContainer}>
-            <InputData
-              hint="이름"
-              onChangeText={handleNameChange}
-              value={inputName}
-            />
-          </View>
-          <View style={[styles.textContainer, { marginTop: 50 }]}>
-            <InputText text="생년월일을 입력해 주세요." />
-          </View>
-          <View style={styles.inputContainer}>
-            <InputData
-              hint="생년월일"
-              onChangeText={handleBirthChange}
-              value={inputBirth}
-            />
-          </View>
-        </View>
 
-        <View style={styles.buttonContainer}>
-          <ButtonBig text="다음" style={lbtnColor} onPress={navigateId} />
+          <View style={styles.buttonContainer}>
+            <ButtonBig text="다음" style={lbtnColor} onPress={navigateId} />
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
