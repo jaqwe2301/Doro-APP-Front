107829364c08d0d1bd507c24de843588fc19db04
Nohsein <si2238@naver.com>
2023-06-30 01:31:00 +0900

    Feat: add mypage alarmEdit

--- utill/http.js
+++ utill/http.js
@@ -263,3 +263,32 @@ export async function updateUserImage({ formData }) {
     throw error;
   }
 }
+
+export async function alarmEdit({ id, notificationAgreement }) {
+  try {
+    const response = await instance.patch(
+      "/users/" + `${id}` + "/notification-settings",
+      { notificationAgreement: notificationAgreement }
+    );
+    console.log(response.data);
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+
+    throw error;
+  }
+}
