107d600446e971d590c28f13d2f8149181eb2262
Nohsein <si2238@naver.com>
2023-06-30 02:28:22 +0900

    Feat: add pushNoti api

--- utill/http.js
+++ utill/http.js
@@ -125,26 +125,28 @@ export async function readNotification({ notificationId }) {
 }
 export async function pushNotification({ body, title }) {
   try {
-    const token = await AsyncStorage.getItem("token");
-
-    console.log(token);
-
-    const response = await axios.post(
-      URL + "/notifications",
-      {
-        body: body,
-        title: title,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          // "Content-Type": "multipart/form-data",
-        },
-      }
-    );
+    const response = await instance.post("/notifications", {
+      title: title,
+      body: body,
+      userIds: [],
+    });
     return response.data;
   } catch (error) {
-    console.log(error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
 
     throw error;
   }
