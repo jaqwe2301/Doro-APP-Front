b028a8cdc0e0169f39de62171e41ce4d711606ab
Nohsein <si2238@naver.com>
2023-07-04 10:35:19 +0900

    Feat: logoutApi - not redirect

--- utill/http.js
+++ utill/http.js
@@ -332,3 +332,27 @@ export async function alarmEdit({ id, notificationAgreement }) {
     throw error;
   }
 }
+
+export async function logout() {
+  try {
+    const response = await instance.post("/logout");
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    throw error;
+  }
+}
