c60893baf6af04f4faff56cbc7209ca5f3867c70
Nohsein <si2238@naver.com>
2023-07-18 20:04:07 +0900

    Edit: generation picker style

--- components/ui/GenerationModal.js
+++ components/ui/GenerationModal.js
@@ -6,10 +6,11 @@ import {
   Modal,
   SafeAreaView,
 } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+// import { Picker } from "@react-native-picker/picker";
 import Modalx from "../../assets/modalx.svg";
 import ButtonBig from "./ButtonBig";
 import { GlobalStyles } from "../../constants/styles";
+import { PickerIOS } from "@react-native-picker/picker";
 
 function GenerationModal({
   setVisibleCode,
@@ -55,7 +56,7 @@ function GenerationModal({
                   </View>
                   <Text style={styles.statusTitle}>{title}</Text>
                 </View>
-                <Picker
+                {/* <Picker
                   selectedValue={inputGeneration}
                   onValueChange={(itemValue, itemIndex) => {
                     setInputGeneration(itemValue);
@@ -90,7 +91,42 @@ function GenerationModal({
                   <Picker.Item label="18" value="18" />
                   <Picker.Item label="19" value="19" />
                   <Picker.Item label="20" value="20" />
-                </Picker>
+                </Picker> */}
+                <PickerIOS
+                  selectedValue={inputGeneration}
+                  onValueChange={(itemValue, itemIndex) => {
+                    setInputGeneration(itemValue);
+                    if (type) {
+                      setSelectCode(itemValue + "기");
+                      setStatusGStyle(styles.textInputText);
+                    }
+                  }}
+                  itemStyle={{
+                    marginHorizontal: 10,
+                  }}
+                  mode="dropdown"
+                >
+                  <PickerIOS.Item label="1" value="1" />
+                  <PickerIOS.Item label="2" value="2" />
+                  <PickerIOS.Item label="3" value="3" />
+                  <PickerIOS.Item label="4" value="4" />
+                  <PickerIOS.Item label="5" value="5" />
+                  <PickerIOS.Item label="6" value="6" />
+                  <PickerIOS.Item label="7" value="7" />
+                  <PickerIOS.Item label="8" value="8" />
+                  <PickerIOS.Item label="9" value="9" />
+                  <PickerIOS.Item label="10" value="10" />
+                  <PickerIOS.Item label="11" value="11" />
+                  <PickerIOS.Item label="12" value="12" />
+                  <PickerIOS.Item label="13" value="13" />
+                  <PickerIOS.Item label="14" value="14" />
+                  <PickerIOS.Item label="15" value="15" />
+                  <PickerIOS.Item label="16" value="16" />
+                  <PickerIOS.Item label="17" value="17" />
+                  <PickerIOS.Item label="18" value="18" />
+                  <PickerIOS.Item label="19" value="19" />
+                  <PickerIOS.Item label="20" value="20" />
+                </PickerIOS>
               </View>
               <View style={{ marginBottom: 34, marginHorizontal: 20 }}>
                 <ButtonBig
