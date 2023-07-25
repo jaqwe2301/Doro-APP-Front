7430b734eb55a646a0fb653f14a5c2c42b9e7411
Nohsein <si2238@naver.com>
2023-07-25 16:33:12 +0900

    Fix: 안드로이드 기수 모달 창 오류
    안드로이드 기수 모달 창 오류 해결
    - 이중 Modal 창이 문제였음

--- components/signUp/Code.js
+++ components/signUp/Code.js
@@ -102,7 +102,7 @@ function Code({ navigation, route }) {
         studentId: signData.studentId,
         studentStatus: signData.studentStatus,
       });
-      
+
       if (response.data.success) {
         navigation.navigate("finish");
       }
@@ -381,62 +381,16 @@ function Code({ navigation, route }) {
             </Pressable>
           </SafeAreaView>
         </Modal>
-
-        <Modal
-          animationType="none"
-          transparent={true}
-          visible={visibleCode}
-          statusBarTranslucent={true}
-          onRequestClose={() => setVisibleCode(!visibleCode)}
-        >
-          <SafeAreaView style={{ flex: 1 }}>
-            <Pressable
-              style={styles.modalOverlay}
-              onPress={() => setVisibleCode(!visibleCode)}
-            >
-              <Pressable>
-                <View
-                  style={{
-                    backgroundColor: "white",
-                    height: 357,
-                    justifyContent: "space-between",
-
-                    borderTopEndRadius: 5.41,
-                    borderTopStartRadius: 5.41,
-                  }}
-                >
-                  <View>
-                    <View style={styles.statusTitleContainer}>
-                      <View style={styles.iconContainer}>
-                        <Pressable onPress={() => setVisibleCode(!visibleCode)}>
-                          <Modalx width={24} height={24} />
-                        </Pressable>
-                      </View>
-                      <Text style={styles.statusTitle}>기수</Text>
-                    </View>
-                  </View>
-                  <GenerationModal
-                    setVisibleCode={setVisibleCode}
-                    visibleCode={visibleCode}
-                    title="기수"
-                    inputGeneration={inputGeneration}
-                    setInputGeneration={setInputGeneration}
-                    setSelectCode={setSelectCode}
-                    setStatusGStyle={setStatusGStyle}
-                    type={true}
-                  />
-                  <View style={{ marginBottom: 34, marginHorizontal: 20 }}>
-                    <ButtonBig
-                      text="확인"
-                      style={GlobalStyles.colors.primaryDefault}
-                      onPress={() => setVisibleCode(false)}
-                    />
-                  </View>
-                </View>
-              </Pressable>
-            </Pressable>
-          </SafeAreaView>
-        </Modal>
+        <GenerationModal
+          setVisibleCode={setVisibleCode}
+          visibleCode={visibleCode}
+          title="기수"
+          inputGeneration={inputGeneration}
+          setInputGeneration={setInputGeneration}
+          setSelectCode={setSelectCode}
+          setStatusGStyle={setStatusGStyle}
+          type={true}
+        />
       </View>
     </SafeAreaView>
   );
