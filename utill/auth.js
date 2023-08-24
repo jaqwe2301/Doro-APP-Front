436de5dad17230127ad959656c82f2756f831d7d
jaqwe2301 <jaqwe2301@naver.com>
2023-08-24 14:45:18 +0900

    Fix: 배포 서버 비공개

---
+++ utill/auth.js
@@ -0,0 +1,237 @@
+import axios from "axios";
+import { URL } from "./config";
+import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import messaging from "@react-native-firebase/messaging";
+import notifee from "@notifee/react-native";
+
+// const URL = "http://10.0.2.2:8080";
+
+export function authPhoneNum({ messageType, phone }) {
+  axios
+    .post(URL + "/message/send", {
+      messageType: messageType,
+      phone: phone,
+    })
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+export async function checkPhoneNum(phone) {
+  try {
+    const response = await axios.get(URL + "/check/phone?phone=" + phone);
+    return response.data.code;
+    // console.log(phone);
+  } catch (err) {
+    if (err.response) {
+      // 서버가 응답을 반환한 경우
+      // console.log(err.response.data.code);
+      return err.response.data.code;
+    } else {
+      // 그 외의 에러
+      Alert.alert("요청 실패", "휴대폰 번호 유효 체크를 실패하였습니다.");
+      console.log("Error", err.message);
+    }
+  }
+}
+
+export async function verifyauthPhoneNum({ authNum, messageType, phone }) {
+  const response = await axios.post(URL + "/message/verify", {
+    authNum: authNum,
+    messageType: messageType,
+    phone: phone,
+  });
+  console.log(response.data);
+  const success = response.data.success;
+
+  return success;
+}
+
+export async function findAccount({ phone }) {
+  const response = await axios.get(URL + "/find/account?phone=" + phone);
+
+  console.log(phone);
+  const code = response.data;
+  console.log(code);
+
+  return code;
+}
+
+export async function checkAccount({ account }) {
+  const response = await axios.get(URL + "/check/account?account=" + account);
+
+  console.log(account);
+  const data = response.data;
+  console.log(data);
+
+  return data;
+}
+
+export async function changePassword({
+  account,
+  newPassword,
+  newPasswordCheck,
+  phone,
+}) {
+  const response = await axios.post(URL + "/change/password", {
+    account: account,
+    newPassword: newPassword,
+    newPasswordCheck: newPasswordCheck,
+    phone: phone,
+  });
+
+  const data = response.data;
+  console.log(data);
+
+  return data;
+}
+
+export async function login({ id, pw }) {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  let fcmToken = null;
+  if (enabled) {
+    console.log("Authorization status:", authStatus);
+    fcmToken = await messaging().getToken();
+    await AsyncStorage.setItem("fcmToken", fcmToken);
+  } else {
+    await AsyncStorage.removeItem("fcmToken");
+  }
+  try {
+    const response = await axios.post(
+      URL + "/login",
+      {
+        account: id,
+        password: pw,
+      },
+      {
+        // headers: fcmToken ? { fcmToken: fcmToken } : undefined,
+        // headers: {
+        //   fcmToken:
+        //     "d_AO9w7NQK2MNVwlpHnXMo:APA91bHZ3DIByv7v6nO_RUfPDSlWSMGNUZGz0cH5lskbkgCxs_09UWi8kBpLIAsWZGr5WEjssOCtESLwT-OSojJTCKjLWdFcSs19h7exD59ExsXll51WrQ7j2-aW-TRGyPf1qUcnMMnt",
+        // },
+      }
+    );
+    const token = response;
+    // 로그인 성공 후, 푸시메시지 수신 리스너 등록
+    if (fcmToken) {
+      const onDisplayNotification = async ({ title = "", body = "" }) => {
+        const channelId = await notifee.createChannel({
+          id: "channelId",
+          name: "channelName",
+        });
+        await notifee.displayNotification({
+          title,
+          body,
+          android: {
+            channelId,
+          },
+        });
+      };
+      messaging().onMessage(async (remoteMessage) => {
+        const title = remoteMessage?.notification?.title;
+        const body = remoteMessage?.notification?.body;
+        await onDisplayNotification({ title, body });
+      });
+    }
+    return token;
+  } catch (error) {
+    Alert.alert("로그인 실패", "로그인에 실패하셨습니다.");
+    // console.error("Error occurred during login: ", error);
+    if (error.response) {
+      // 서버가 응답을 반환한 경우
+      console.log("Error response:", error.response.data);
+    } else if (error.request) {
+      // 요청이 만들어졌지만, 응답을 받지 못한 경우
+      console.log("Error request:", error.request);
+    } else {
+      // 그 외의 에러
+      console.log("Error", error.message);
+    }
+  }
+}
+
+export async function reToken({ accessToken, refreshToken }) {
+  const response = await axios.post(URL + "/reissue", {
+    accessToken: accessToken,
+    refreshToken: refreshToken,
+  });
+
+  // const token = response.headers.authorization;
+  console.log(response);
+
+  // console.log("hihi\t");
+  // console.log(token);
+
+  return response;
+}
+
+export async function signUp({
+  account,
+  birth,
+  doroAuth,
+  gender,
+  generation,
+  major,
+  name,
+  notificationAgreement,
+  password,
+  passwordCheck,
+  phone,
+  role,
+  school,
+  studentId,
+  studentStatus,
+}) {
+  try {
+    console.log(
+      account,
+      birth,
+      doroAuth,
+      gender,
+      generation,
+      major,
+      name,
+      notificationAgreement,
+      password,
+      passwordCheck,
+      phone,
+      role,
+      school,
+      studentId,
+      studentStatus
+    );
+    const response = await axios.post(URL + "/join", {
+      account: account,
+      birth: birth,
+      doroAuth: doroAuth,
+      gender: gender,
+      generation: generation,
+      major: major,
+      name: name,
+      notificationAgreement: notificationAgreement,
+      password: password,
+      passwordCheck: passwordCheck,
+      phone: phone,
+      role: role,
+      school: school,
+      studentId: studentId,
+      studentStatus: studentStatus,
+    });
+    console.log(response.data);
+    if (!response.data.success) {
+      Alert.alert("회원가입 오류", response.data.message);
+    }
+    return response;
+  } catch (error) {
+    console.log(error);
+    console.log("여긴가");
+  }
+}
---
+++ utill/http.js
@@ -0,0 +1,464 @@
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Interceptor from "./Interceptor";
+import { URL } from "./config";
+
+// const URL = "http://10.0.2.2:8080";
+const instance = Interceptor();
+
+// id를 넣어주면 header 필요없고 id없으면 header 필요하다
+export async function getProfile({ id }) {
+  try {
+    const response = await instance.get("/users/" + `${id}`);
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error + "api er");
+
+    throw error;
+  }
+}
+
+//findAllUser를 구현해버렸네
+
+export async function getProfile2({
+  generation,
+  major,
+  phone,
+  school,
+  studentId,
+  studenStatus,
+  id,
+}) {
+  try {
+    const response = await instance.get("/users/" + `${id}`, {
+      params: {
+        generation: generation,
+        major: major,
+        phone: phone,
+        school: school,
+        studentId: studentId,
+        studenStatus: studenStatus,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateProfile({
+  birth,
+  generation,
+  major,
+  phone,
+  school,
+  studentId,
+  studentStatus,
+  id,
+}) {
+  try {
+    const response = await instance.patch("/users/" + `${parseInt(id)}`, {
+      birth: birth,
+      generation: parseInt(generation),
+      major: major,
+      phone: phone,
+      school: school,
+      studentId: studentId,
+      studentStatus: studentStatus,
+    });
+    console.log(
+      parseInt(generation),
+      major,
+      phone,
+      school,
+      studentId,
+      studentStatus,
+      parseInt(id)
+    );
+    return response.data;
+  } catch (error) {
+    console.log(
+      parseInt(generation),
+      major,
+      phone,
+      school,
+      studentId,
+      studentStatus,
+      parseInt(id)
+    );
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function checkAccount({ account }) {
+  const response = await axios.get(URL + "/check/account?account=" + account);
+
+  console.log(account);
+  const data = response.data;
+  console.log(data);
+
+  return data;
+}
+
+export async function getNotification({ userId, page, size }) {
+  try {
+    const response = await instance.get(
+      URL +
+        "/notifications/" +
+        `${userId}` +
+        "?page=" +
+        `${page}` +
+        "&size=" +
+        `${size}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function readNotification({ notificationId }) {
+  try {
+    const response = await instance.post(
+      "/notifications/" + `${notificationId}` + "/doRead"
+    );
+    return response;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function pushNotification({ body, title }) {
+  try {
+    const response = await instance.post("/notifications", {
+      title: title,
+      body: body,
+      // userIds: [],
+    });
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+
+    throw error;
+  }
+}
+
+export async function getAnnouncement({ page, size }) {
+  try {
+    const response = await instance.get(
+      "/announcements?page=" + page + "&size=" + size
+    );
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function getAnnouncementId({ id }) {
+  try {
+    const response = await instance.get("/announcements/" + `${id}`);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function createAnnouncement({ formData, title, body }) {
+  try {
+    const token = await AsyncStorage.getItem("token");
+
+    console.log("여긴 http파일");
+    // const announcementReq = new Blob(
+    //   [JSON.stringify({ title: title, body: body })],
+    //   { type: "application/json" }
+    // );
+    // const formData = new FormData();
+    // formData.append("announcementReq", {
+    //   title: title,
+    //   body: body,
+    //   writer: "노세인",
+    // });
+    // const obj = { title: title, body: body, writer: "노세인" };
+    // const blob = new Blob([JSON.stringify(obj)], {
+    //   type: "application/json",
+    // });
+    // formData.append("announcementReq", JSON.stringify([obj]));
+    // formData.append("announcementReq", blob);
+    console.log("폼데이터" + JSON.stringify(formData));
+    const boundary = "----ExpoBoundary" + Math.random().toString(16).slice(2);
+    const response = await axios
+      .post(
+        URL + "/announcements",
+        // { announcementReq: announcementReq, picture: formData },
+        // formData
+        {
+          // announcementReq: {
+          announcementReq: JSON.stringify({
+            title: title,
+            body: body,
+            writer: "노세인",
+            // },
+          }),
+        },
+        // formData,
+        /// formData,
+        {
+          headers: {
+            Accept: "application/json",
+            "content-type": `multipart/form-data; boundary=${boundary}`,
+            //"Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${token}`,
+          },
+          //transformRequest: (data, header) => data,
+          transformRequest: [
+            function (data, headers) {
+              // Do whatever you want to transform the data
+              return JSON.stringify(data);
+            },
+          ],
+        }
+      )
+      .then((res) => {
+        console.log("then 리턴" + res);
+        return res;
+      })
+      .catch((e) => {
+        console.log("post 내 에러" + e);
+      });
+    console.log(response);
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(JSON.stringify(error.response.headers) + "response");
+    }
+    if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(JSON.stringify(error.request) + "리퀘스트");
+    }
+    // Something happened in setting up the request that triggered an Error
+    console.log("Error", error.message);
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function createAnnouncement2({ formData }) {
+  try {
+    const response = await instance.post("/announcements", formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(JSON.stringify(error.response.headers) + "response");
+    }
+    if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(JSON.stringify(error.request) + "리퀘스트");
+    }
+    // Something happened in setting up the request that triggered an Error
+    console.log("Error", error.message);
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function editAnnouncement({ formData, id }) {
+  try {
+    const response = await instance.patch(
+      "/announcements/" + `${id}`,
+      formData,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function deleteAnnouncement({ id }) {
+  try {
+    const response = await instance.delete("/announcements/" + `${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function deleteUser() {
+  try {
+    const response = await instance.delete("/withdrawal");
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function updateUserImage({ formData }) {
+  try {
+    console.log(formData);
+    const response = await instance.patch("/users/profile", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      //  transformRequest: () => formData,
+    });
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+
+    throw error;
+  }
+}
+
+export async function alarmEdit({ id, notificationAgreement }) {
+  try {
+    const response = await instance.patch(
+      "/users/" + `${id}` + "/notification-settings",
+      { notificationAgreement: notificationAgreement }
+    );
+    console.log(response.data);
+    return response;
+  } catch (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+
+    throw error;
+  }
+}
+
+export async function logout() {
+  try {
+    const fcmToken = await AsyncStorage.getItem("fcmToken");
+    // console.log(fcmToken + "로그아웃 fcm");
+    const response = await instance.post("/logout", undefined, {
+      headers: {
+        fcmToken: fcmToken,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.log("로그아웃 에러", error);
+    throw error;
+  }
+}
+
+export async function getCityList({ status }) {
+  try {
+    const response = await instance.get("/lectures/cities/" + `${status}`);
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+
+export async function getLectureList({
+  city,
+  endDate,
+  lectureStatus,
+  startDate,
+  page,
+  size,
+}) {
+  try {
+    const res = await instance.get("/lectures", {
+      params: {
+        city: city,
+        endDate: endDate,
+        startDate: startDate,
+        page: page,
+        size: size,
+        lectureStatus: lectureStatus,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data.data; // 프로미스에서 결과를 반환
+  } catch (err) {
+    console.log("에러났나염?>?");
+    if (err.response) {
+      // 서버가 응답을 반환한 경우
+      console.log("Error response:", err.response.data);
+    } else if (err.request) {
+      // 요청이 만들어졌지만, 응답을 받지 못한 경우
+      console.log("Error request:", err.request);
+    } else {
+      // 그 외의 에러
+      console.log("Error", err.message);
+    }
+    throw err; // 에러를 다시 던져서 호출자에게 전달
+  }
+}
