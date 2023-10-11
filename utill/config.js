5363169acde7f4a0fd47a6f7da8a61e6c8d9e1c0
jaqwe2301 <jaqwe2301@naver.com>
2023-10-11 23:47:32 +0900

    Fix: env 방식 변경

--- utill/config.js
+++ utill/config.js
@@ -1,3 +1,5 @@
-export const URL = process.env.REACT_APP_URL;
+import { REACT_APP_URL } from "@env";
+
+export const URL = REACT_APP_URL;
 // export const URL = "http://10.0.2.2:8080";
 // export const URL = "http://172.30.1.67:8080";
